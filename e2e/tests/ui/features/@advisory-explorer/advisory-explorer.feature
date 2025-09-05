50fd74352f934a6a01c3705e9dcd28663a16ba4c
Vilém Obrátil <41943007+vobratil@users.noreply.github.com>
2025-09-05 14:17:19 +0200

    tests: Add Advisory UI tests (#697)
    Signed-off-by: Vilem Obratil <vobratil@redhat.com>

--- e2e/tests/ui/features/@advisory-explorer/advisory-explorer.feature
+++ e2e/tests/ui/features/@advisory-explorer/advisory-explorer.feature
@@ -2,6 +2,61 @@ Feature: Advisory Explorer
     Background: Authentication
         Given User is authenticated
 
+# Search for advisories
+    Scenario: Search for an advisory using the general search bar
+        
+        When User searches for an advisory named "<advisoryID>" in the general search bar
+        
+        Then The advisory "<advisoryID>" shows in the results
+
+        Examples:
+            | advisoryID      |
+            | CVE-2024-26308  |
+    
+    Scenario: Search for an advisory using the dedicated search bar
+
+        When User searches for "<advisoryID>" in the dedicated search bar
+        
+        Then The advisory "<advisoryID>" shows in the results
+
+        Examples:
+            | advisoryID      |
+            | CVE-2024-26308  |
+
+    # Advisory Explorer
+    Scenario: Display an overview of an advisory
+
+        When User visits Advisory details Page of "<advisoryID>"
+
+        Then The page title is "<advisoryID>"
+        Then The "Download" action is available
+
+        Examples:
+            | advisoryID      |
+            | CVE-2024-26308  |
+
+    Scenario: Download an advisory
+
+        When User visits Advisory details Page of "<advisoryID>"
+
+        Then "Download Advisory" action is invoked and downloaded filename is "<fileName>"
+
+        Examples:
+            | advisoryID      | fileName            |
+            | CVE-2024-26308  | CVE-2024-26308.json |
+
+    Scenario: Display the Info tab
+
+        When User visits Advisory details Page of "<advisoryID>"
+
+        Then The "Overview" panel is visible
+        Then The "Publisher" panel is visible
+        Then The "Tracking" panel is visible
+
+        Examples:
+            | advisoryID      |
+            | CVE-2024-26308  |
+
 # Advisory Vulnerabilities
 Scenario: Display vulnerabilities tied to a single advisory
     Given User visits Advisory details Page of "<advisoryName>" with type "<advisoryType>"
--- e2e/tests/ui/steps/details-page.ts
+++ e2e/tests/ui/steps/details-page.ts
@@ -1,11 +1,42 @@
 import { createBdd } from "playwright-bdd";
 import { DetailsPage } from "../helpers/DetailsPage";
+import { expect } from "@playwright/test";
 
 export const { Given, When, Then } = createBdd();
 
 Then("The page title is {string}", async ({ page }, title) => {
   const pageWithTabs = new DetailsPage(page);
   await pageWithTabs.verifyPageHeader(title);
+});
+
+Then("The {string} action is available", async ({ page }, actionName) => {
+  const detailsPage = new DetailsPage(page);
+  await detailsPage.verifyActionIsAvailable(actionName);
+});
+
+Then(
+  "{string} action is invoked and downloaded filename is {string}",
+  async ({ page }, actionName, expectedFilename) => {
+    const downloadPromise = page.waitForEvent("download");
+
+    const detailsPage = new DetailsPage(page);
+    await detailsPage.clickOnPageAction(actionName);
+
+    const download = await downloadPromise;
+
+    const filename = download.suggestedFilename();
+    expect(filename).toEqual(expectedFilename);
+  },
+);
+
+Then("The {string} button is visible", async ({ page }, buttonName) => {
+  const detailsPage = new DetailsPage(page);
+  await detailsPage.verifyButtonIsVisible(buttonName);
+});
+
+Then("The {string} panel is visible", async ({ page }, panelName) => {
+  const detailsPage = new DetailsPage(page);
+  await detailsPage.verifyPanelIsVisible(panelName);
 });
 
 Then("Tab {string} is selected", async ({ page }, tabName) => {
@@ -26,4 +57,14 @@ Then("Tab {string} is not visible", async ({ page }, tabName) => {
 When("User selects the Tab {string}", async ({ page }, tabName) => {
   const detailsPage = new DetailsPage(page);
   await detailsPage.selectTab(tabName);
+});
+
+When("User selects the {string} action", async ({ page }, menuItemName) => {
+  const detailsPage = new DetailsPage(page);
+  await detailsPage.clickOnPageAction(menuItemName);
+});
+
+When("User clicks the {string} button", async ({ page }, buttonName) => {
+  const detailsPage = new DetailsPage(page);
+  await detailsPage.clickOnPageButton(buttonName);
 });
