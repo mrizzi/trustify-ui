16c12dfdd28a921f3df9327c5aab58764b6b4e48
mrizzi <mrizzi@users.noreply.>
2026-06-23 11:21:59 +0000

    Checkin dist dir

--- client/dist/assets/ModalFooter-BfyEbRrP.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./react-B-P7c96U.js";import{k as t,w as n}from"./Bullseye-DYUzMYzf.js";import{Y as r}from"./index-CZEHcSai.js";var i=e(),a=e=>{var{children:a,className:o}=e,s=t(e,[`children`,`className`]);return(0,i.jsx)(`footer`,Object.assign({},s,{className:n(r.modalBoxFooter,o),children:a}))};a.displayName=`ModalFooter`;export{a as t};
